ef31617a15e44dd68785d452cebf6f4e0854a567
Lisias T <me@lisias.net>
2025-04-03 21:06:47 -0300

    Legal Mambo Jambo Haveli Edition

--- Source/KspWalkAbout/Startup.cs
+++ Source/KspWalkAbout/Startup.cs
@@ -26,7 +26,9 @@ using System;
 
 using UnityEngine;
 
-namespace KourageousTourists
+using KSPe.Common.Dialogs;
+
+namespace KspWalkAbout
 {
 	[KSPAddon(KSPAddon.Startup.Instantly, true)]
 	internal class Startup : MonoBehaviour
@@ -53,13 +55,12 @@ namespace KourageousTourists
 			{
 				try
 				{
-					this.LoadSupportForChutes(a);
 					this.LoadSupportForEVA(a);
 				}
 				catch (System.DllNotFoundException e)
 				{
 					Log.error(e.ToString());
-					GUI.MissingDLLAlertBox.Show(e.Message);
+					GUI.MissingDLLErrorBox.Show(e.Message);
 				}
 				catch (System.Exception e)
 				{
@@ -69,64 +70,19 @@ namespace KourageousTourists
 			}
 		}
 
-		private void LoadSupportForChutes(KSPe.Util.SystemTools.Assembly.Loader<Startup> a)
-		{
-			if (KSPe.Util.KSP.Version.Current >= KSPe.Util.KSP.Version.FindByVersion(1,4,0))
-			{
-				if (KSPe.Util.SystemTools.Type.Finder.ExistsByQualifiedName("VanguardTechnologies.ModuleKrKerbalParachute"))
-				{
-					Log.info("Loading Chute Support for KSP >= 1.4 and Vanguard Technologies");
-					a.LoadAndStartup("KourageousTourists.KSP.Chute.14.VanguardTechnologies");
-				}
-				else if (KSPe.Util.SystemTools.Type.Finder.ExistsByQualifiedName("RealChute.RealChuteModule"))
-				{
-					Log.info("Loading Chute Support for KSP >= 1.4 and Real Chutes");
-					a.LoadAndStartup("KourageousTourists.KSP.Chute.14.RealChute");
-				}
-				else
-				{
-					Log.info("Loading Chute Support for KSP 1.4 Stock");
-					a.LoadAndStartup("KourageousTourists.KSP.Chute.14");
-				}
-			}
-			else if (KSPe.Util.KSP.Version.Current >= KSPe.Util.KSP.Version.FindByVersion(1,3,0))
-			{
-				if (KSPe.Util.SystemTools.Type.Finder.ExistsByQualifiedName("VanguardTechnologies.ModuleKrKerbalParachute"))
-				{
-					Log.info("Loading Chute Support for KSP >= 1.3 and Vanguard Technologies");
-					a.LoadAndStartup("KourageousTourists.KSP.Chute.13.VanguardTechnologies");
-				}
-				else if (KSPe.Util.SystemTools.Type.Finder.ExistsByQualifiedName("RealChute.RealChuteModule"))
-				{
-					Log.info("Loading Chute Support for KSP 1.3.x and Real Chutes");
-					a.LoadAndStartup("KourageousTourists.KSP.Chute.13.RealChute");
-				}
-				else throw new DllNotFoundException("You need to install Vanguard Technologies, RealChutes on KSP 1.3 for playing Kourageous Tourists /L");
-			}
-			else throw new NotSupportedException("Your current KSP installment is not supported by Kourageous Tourists /L");
-		}
-
 		private void LoadSupportForEVA(KSPe.Util.SystemTools.Assembly.Loader<Startup> a)
 		{
-			if (KSPe.Util.KSP.Version.Current >= KSPe.Util.KSP.Version.FindByVersion(1,6,0))
+			if (KSPe.Util.KSP.Version.Current >= KSPe.Util.KSP.Version.FindByVersion(1,11,0))
 			{
-				Log.info("Loading EVA Support for [KSP >= 1.6]");
-				a.LoadAndStartup("KourageousTourists.KSP.EVA.16");
+				Log.info("Loading EVA Support for [KSP >= 1.11]");
+				a.LoadAndStartup("KspWalkAbout.KSP.EVA.11");
 			}
 			else if (KSPe.Util.KSP.Version.Current >= KSPe.Util.KSP.Version.FindByVersion(1,3,0))
 			{
-				if (null != Type.GetType("KIS.KIS, KIS", false)) // check!
-				{
-					Log.info("Loading EVA Support for [1.3 <= KSP < 1.6] and KIS");
-					a.LoadAndStartup("KourageousTourists.KSP.EVA.13.KIS");
-				}
-				else
-				{
-					Log.info("Loading Chute Support for [1.3 <= KSP < 1.6] Stock");
-					a.LoadAndStartup("KourageousTourists.KSP.EVA.13");
-				}
+				Log.info("Loading Chute Support for [1.3 <= KSP < 1.11]");
+				a.LoadAndStartup("KspWalkAbout.KSP.EVA.11");
 			}
-			else throw new NotSupportedException("Your current KSP installment is not supported by Kourageous Tourists /L");
+			else throw new NotSupportedException("Your current KSP installment is not supported by Walk About /L");
 		}
 	}
 }
